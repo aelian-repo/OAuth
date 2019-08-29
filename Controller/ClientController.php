7dc451d63be81c4030e9904f7214ee082cd79214
joacir <jometal@gmail.com>
2019-08-29 18:01:40 -0300

    Exemplos comentados

--- Controller/ClientController.php
+++ Controller/ClientController.php
@@ -1,48 +1,99 @@
 <?php
+/**
+ * Controller de exemplo para REST Client
+ * 
+ * Ver tambem:
+ *    
+ *    Config/routes.php para exemplo de configuração das rotas de REST Client
+ * 
+ */
+
 App::uses('OAuthAppController', 'OAuth.Controller');
 
 class ClientController extends OAuthAppController {
 
     public $components = array('RequestHandler');
 
-    public $autoRender = false;
-    public $id = null;
-    public $accessToken = '';
+    /**
+     * Não tem views, só exibe URLs e Responses do REST Server
+     */
+    public $autoRender = false; 
+    /**
+     * Id do Resource de Exemplo
+     */
+    public $id = null; 
+    /**
+     * Armazena o AccessToken para poder acessar o Resource do REST Server
+     */
+    public $accessToken = ''; 
 
 	public function beforeFilter() {
+        /**
+         * Retirado o Auth só pra teste, mas em Apps reais pode ser necessário manter.
+         */
         $this->Components->unload('Auth');
+        /**
+         * Se ainda não tiver AccessToken, faz a autenticação no REST Server
+         */
         if (empty($this->accessToken)) {
             $this->getAccessToken();
         }
     }
     
+    /**
+     * Faz a autenticação no REST Server
+     */
     private function getAccessToken() {
+        /**
+         * Credencial do Client.
+         * Substituir por informações do client que quer testar.
+         * Para criar um novo Client:
+         * 
+         *      /api/v1/oauth/add
+         * 
+         */
         $credencial = array(
             'response_type' => 'code',
             'client_id' => 'NWQ2Njg4MWIxNjRmNWY5', 
             'client_secret' => '62be871736ed29ebbf1ef055b058df23400bf432'
         );
+        /**
+         * Passo 1 - Solicita Authorization
+         */
         $authorization = $this->authorizationRequest('authorize', $credencial);
         pr($authorization);
         
+        /**
+         * Autorização do Client
+         * Já reaproveita informações da credencial do Client e Adiciona código de Autorização         * 
+         */
         $authorization = array(
             'grant_type' => 'authorization_code',
             'code' => $authorization['code'],
             'client_id' => $credencial['client_id'], 
             'client_secret' => $credencial['client_secret']
         );
+        /**
+         * Passo 2 - Solicita AccessToken 
+         */
         $token = $this->authorizationRequest('token', $authorization);
         pr($token);
 
         $this->accessToken = $token['access_token'];
     }
 
+    /** 
+     * Simulação de GET de Resources
+     */
     public function index() {
         $response = $this->request('GET');
         pr('INDEX');
         pr($response);
     }
 
+    /** 
+     * Simulação de GET de Resource por ID
+     */
     public function view($id = null) {
         $this->id = $id;
         $response = $this->request('GET');
@@ -50,6 +101,9 @@ class ClientController extends OAuthAppController {
         pr($response);
     }
 
+    /** 
+     * Simulação de POST de Resource
+     */
     public function add() {
         $this->id = null;
         $resource = array('id' => '4', 'name' => 'milho');
@@ -58,6 +112,9 @@ class ClientController extends OAuthAppController {
         pr($response);
     }
 
+    /** 
+     * Simulação de POST de Resource por ID (Update)
+     */
     public function edit($id = null) {
         $this->id = $id;
         $resource = array('id' => $id, 'name' => 'beterraba');
@@ -66,13 +123,19 @@ class ClientController extends OAuthAppController {
         pr($response);
     }
 
+    /** 
+     * Simulação de DELETE de Resource por ID
+     */
     public function delete($id = null) {
         $this->id = $id;
         $response = $this->request('DELETE');
         pr('DELETE');
         pr($response);
     }
-    
+
+    /**
+     * Faz as requisições para autorização no REST Server
+     */
     private function authorizationRequest($method, $credencial) {
         $url = Router::url('/api/v1/oauth/' . $method, true) . '?' . http_build_query($credencial);
         pr($url);
@@ -95,6 +158,9 @@ class ClientController extends OAuthAppController {
         return $response;                
     }       
 
+    /**
+     * Faz as requisições para os recursos no REST Server
+     */
     private function request($method, $resource = array()) {
         $resource = json_encode($resource);                                                                   
         $url = Router::url('/api/v1/server/', true) . $this->id . '?access_token=' . $this->accessToken;
@@ -111,6 +177,9 @@ class ClientController extends OAuthAppController {
         curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                           
            'Content-Type: application/json',   
            'Content-Length: ' . strlen($resource),                                                        
+            /**
+             * Pode optar por informar o AccessToken aqui e retirar da $url
+             */
 //           'authorization: Bearer ' . $this->accessToken,                                                      
         ));                                                                                           
         $response = curl_exec($curl);
--- Controller/ServerController.php
+++ Controller/ServerController.php
@@ -1,14 +1,28 @@
 <?php
+/**
+ * Controller de exemplo para REST Server
+ * 
+ * Ver tambem:
+ *    
+ *    Config/routes.php para exemplo de configuração das rotas de REST Server
+ * 
+ */
 App::uses('OAuthAppController', 'OAuth.Controller');
 
 class ServerController extends OAuthAppController {
 
     public $components = array(
+        /**
+         * substituir userModel pelo model que vai identificar o usuário no app. Ex: Empresa, Orgao, Singular...
+         */
         'OAuth.OAuth' => array('authenticate' => array('userModel' => 'Singular')), 
         'Session', 
         'RequestHandler'
     );
 
+    /**
+     * Resources de Exemplo
+     */
     public $resources = array(
         '1' => array('id' => 1, 'name' => 'batata'),
         '2' => array('id' => 2, 'name' => 'cenouras'),
@@ -16,19 +30,44 @@ class ServerController extends OAuthAppController {
     );
 
 	public function beforeFilter() {
-        $this->Components->unload('Auth');
+        /**
+         * Não utilizar o Auth porque o OAuth será utilizado no lugar
+         */
+        $this->Components->unload('Auth');        
 	}
 
+    /**
+     * Retorna todos resources
+     * 
+     * url /api/server
+     * method GET 
+     * return resources;
+     */
     public function index() {
         $resources = $this->resources;
         $this->set(array('resources' => $resources, '_serialize' => 'resources'));
     }
 
+    /**
+     * Retorna resource por id
+     * 
+     * url /api/server/[id]
+     * method GET 
+     * return resource;
+     */
     public function view($id = null) {
         $resource = $this->resources[$id];
         $this->set(array('resource' => $resource, '_serialize' => 'resource'));
     }
 
+    /**
+     * Grava novo resource
+     * 
+     * url /api/server
+     * method POST
+     * data [id, name]
+     * return resources;
+     */
     public function add() {
         if (!empty($this->request->data)) {
             $this->resources[$this->request->data['id']] = $this->request->data;
@@ -37,6 +76,14 @@ class ServerController extends OAuthAppController {
         $this->set(array('resources' => $resources, '_serialize' => 'resources'));        
     }
 
+    /**
+     * Atualiza resource por id
+     * 
+     * url /api/server/[id]
+     * method POST
+     * data [id, name]
+     * return resources;
+     */
     public function edit($id = null) {
         if (!empty($this->request->data)) {
             $this->resources[$id] = $this->request->data;
@@ -45,6 +92,13 @@ class ServerController extends OAuthAppController {
         $this->set(array('resources' => $resources, '_serialize' => 'resources'));        
     }
 
+    /**
+     * Delete resource por id
+     * 
+     * url /api/server/[id]
+     * method DELETE
+     * return resources;
+     */
     public function delete($id = null) {
         unset($this->resources[$id]);
         $resources = $this->resources;
